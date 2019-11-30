0a67d7dc29f373a2d4d4bf414e78301ccffead50
gitkiselev <482003@mail.ru>
2019-11-30 16:08:07 +0300

    HW17 done basic for checking

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
+import {connect} from 'react-redux';
 import './app-header.scss';
 import {Link} from "react-router-dom";
 
 
-const AppHeader = ({total}) => {
+const AppHeader = ({items}) => {
+    const total = items.reduce((preVal, item) => (preVal + item.num * item.price), 0)
     return (
         <header className="header">
             <Link className="header__link"
@@ -25,4 +27,12 @@ const AppHeader = ({total}) => {
     )
 };
 
-export default AppHeader;
+const mapStateToProps = ({items}) => {
+    return {
+      items
+    }
+  }
+  
+  const mapDispatchToProps = {};
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(AppHeader);
--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -3,6 +3,7 @@ import './cart-table.scss';
 import {connect} from 'react-redux';
 import {deleteFromCart} from '../../actions';
 
+
 const CartTable = ({items, deleteFromCart}) => {
     return (
         <>
@@ -10,13 +11,15 @@ const CartTable = ({items, deleteFromCart}) => {
             <div className="cart__list">
                 {
                     items.map(item => {
-                        const {title, price, url, id} = item;
+                        const {title, price, url, id, num} = item;
                         return (
                             <div key={id} className="cart__item">
                                 <img src={url} className="cart__item-img" alt={title}></img>
                                 <div className="cart__item-title">{title}</div>
                                 <div className="cart__item-price">{price}$</div>
+                                
                                 <div onClick={() => deleteFromCart(id)} className="cart__close">&times;</div>
+                                <div className="cart__num" style={num < 2 ? {display: 'none' } : {display: 'flex' }}>{num}</div>
                             </div>
                         )
                     })
